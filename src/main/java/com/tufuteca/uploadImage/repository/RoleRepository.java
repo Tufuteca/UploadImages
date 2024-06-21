b3ceba47fcedc9b75a80a5cf351837b74d2ceb88
Tufuteca <tufutecalive@gmail.com>
2024-06-21 11:09:28 +0700

    Исправлены ошибки при создании репозиториев

--- src/main/java/com/tufuteca/uploadImage/repository/RoleRepository.java
+++ src/main/java/com/tufuteca/uploadImage/repository/RoleRepository.java
@@ -1,10 +1,10 @@
 package com.tufuteca.uploadImage.repository;
 
 import com.tufuteca.uploadImage.model.Role;
-import jakarta.persistence.Entity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-@Entity
+@Repository
 public interface RoleRepository extends JpaRepository<Role,Long> {
 
 }
