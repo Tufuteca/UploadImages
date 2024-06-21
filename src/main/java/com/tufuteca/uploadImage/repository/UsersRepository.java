2a6f6dc1089085886b056c3758ecb5fb8d7d9907
Tufuteca <tufutecalive@gmail.com>
2024-06-21 10:22:47 +0700

    Добавлен импорт

--- src/main/java/com/tufuteca/uploadImage/repository/UsersRepository.java
+++ src/main/java/com/tufuteca/uploadImage/repository/UsersRepository.java
@@ -1,5 +1,6 @@
 package com.tufuteca.uploadImage.repository;
 
+import com.tufuteca.uploadImage.model.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
