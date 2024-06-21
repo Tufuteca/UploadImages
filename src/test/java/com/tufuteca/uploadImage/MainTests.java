4cda779302257ddd5b2d4b3bfffb8d7461a60ac6
Tufuteca <tufutecalive@gmail.com>
2024-06-21 10:01:59 +0700

    Проект создан

---
+++ src/test/java/com/tufuteca/uploadImage/MainTests.java
@@ -0,0 +1,13 @@
+package com.tufuteca.uploadImage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
