2d04734da2a816a0824637c77174f9dd0dd9cb69
Tufuteca <tufutecalive@gmail.com>
2024-06-21 12:12:08 +0700

    Добавлены зависимости, изменен navbar, добавлен getMapping для index страницы

--- src/main/java/com/tufuteca/uploadImage/controller/ApplicationController.java
+++ src/main/java/com/tufuteca/uploadImage/controller/ApplicationController.java
@@ -1,4 +1,14 @@
 package com.tufuteca.uploadImage.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class ApplicationController {
+
+    @GetMapping("/")
+    public String getIndexPage(Model model){
+        return "index";
+    }
 }
