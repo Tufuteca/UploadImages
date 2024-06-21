07a8cf1a02224171c63d6ec2e28aac3d1a23f55d
Tufuteca <tufutecalive@gmail.com>
2024-06-21 15:31:25 +0700

    Добавлено добавление администратора

--- src/main/java/com/tufuteca/uploadImage/components/DataInitializer.java
+++ src/main/java/com/tufuteca/uploadImage/components/DataInitializer.java
@@ -1,17 +1,26 @@
 package com.tufuteca.uploadImage.components;
 
+import com.tufuteca.uploadImage.config.SecurityConfig;
 import com.tufuteca.uploadImage.model.Role;
+import com.tufuteca.uploadImage.model.Users;
 import com.tufuteca.uploadImage.repository.RoleRepository;
+import com.tufuteca.uploadImage.repository.UsersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Component
 public class DataInitializer implements CommandLineRunner {
 
     @Autowired
     private RoleRepository roleRepository;
+    @Autowired
+    private UsersRepository usersRepository;
+    @Autowired
+    private SecurityConfig securityConfig;
 
     @Override
     @Transactional
@@ -21,6 +30,19 @@ public class DataInitializer implements CommandLineRunner {
             Role userRole = new Role(2L, "ROLE_USER");
             roleRepository.save(adminRole);
             roleRepository.save(userRole);
+        }
+        if(usersRepository.count() ==0){
+            Users admin = new Users();
+            admin.setLogin("admin");
+            admin.setPassword(securityConfig.passwordEncoder().encode("admin"));
+            Optional<Role> roleOptional = roleRepository.findById(1L);
+            if (roleOptional.isPresent()) {
+                Role role = roleOptional.get();
+                admin.setRole(role);
+                usersRepository.save(admin);
+            }else{
+                System.out.println("Роль не найдена");
+            }
         }
     }
 }
