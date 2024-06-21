c381c85159c1dd6957d92d4210584d0c5664fa67
Tufuteca <tufutecalive@gmail.com>
2024-06-21 15:34:30 +0700

    Перенос перехода по страницам в ApplicationController

--- src/main/java/com/tufuteca/uploadImage/controller/ApplicationController.java
+++ src/main/java/com/tufuteca/uploadImage/controller/ApplicationController.java
@@ -1,5 +1,7 @@
 package com.tufuteca.uploadImage.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,4 +13,15 @@ public class ApplicationController {
     public String getIndexPage(Model model){
         return "index";
     }
+
+    @GetMapping("/authorization")
+    public String getAuthorizationPage(){
+        return "authorization";
+    }
+
+    @GetMapping("/register")
+    public String getRegistrationPage(){
+        return "registration";
+    }
+
 }
