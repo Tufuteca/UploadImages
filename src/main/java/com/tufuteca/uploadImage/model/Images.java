bc6078a938cc1cfc2c15fdec7edf442c17f77741
Tufuteca <tufutecalive@gmail.com>
2024-06-21 15:33:34 +0700

    Разработан класс сущности Images

--- src/main/java/com/tufuteca/uploadImage/model/Images.java
+++ src/main/java/com/tufuteca/uploadImage/model/Images.java
@@ -1,4 +1,28 @@
 package com.tufuteca.uploadImage.model;
 
+import javax.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "images")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Images {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idImage")
+    private Long id;
+
+    @Column(name = "date_image_added")
+    private LocalDateTime dateImageAdded;
+
+    @Lob
+    @Column(name = "image_data", columnDefinition = "bytea")
+    private byte[] imageData;
 }
