17c48c932aeee800955575a3d1c49b716cc641e7
Tufuteca <tufutecalive@gmail.com>
2024-06-22 10:16:21 +0700

    Исправлен баг регистрации

--- src/main/java/com/tufuteca/uploadImage/config/SecurityConfig.java
+++ src/main/java/com/tufuteca/uploadImage/config/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/","/authorization", "/register", "/perform_login", "/css/**", "/js/**").permitAll()
+                .antMatchers("/","/authorization", "/register", "/perform_login", "/css/**", "/js/**","/registration").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
